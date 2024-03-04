68796ff1e97f57bd235b2f3c8eb297ad281d43fa
Rh4096 <rhelvetican@gmail.com>
2024-03-04 11:45:20 +0700

    config

---
+++ src/config.rs
@@ -0,0 +1,51 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Root {
+    pub server: Server,
+    pub version: Version,
+    pub user_config: UserConfig,
+    pub char_config: CharConfig,
+    pub assist_units: Vec<Value>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Server {
+    pub address: String,
+    pub port: u16,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Version {
+    pub android: Android,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Android {
+    pub res_version: String,
+    pub client_version: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct UserConfig {
+    pub restore: Restore,
+    pub secretary: String,
+    pub secretary_skin: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Restore {
+    pub integrated_strategies: bool,
+    pub squad_and_favs: bool,
+    pub ui: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct CharConfig {
+    pub favor_point: u32,
+    pub potential_rank: u8,
+    pub skill_level: u8,
+    pub level: u16,
+    pub evolve_phase: u8,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod config;
+mod routing;
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
