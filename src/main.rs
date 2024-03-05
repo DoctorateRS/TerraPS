6313fafc672903484fd1f8766da6a7efc99e654f
Rh4096 <rhelvetican@gmail.com>
2024-03-05 12:25:28 +0700

    cd

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@ use config::load_config;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
     let config = match load_config() {
         Ok(config) => config,
         Err(e) => panic!("Cannot load config: {}", e),
     };
-    println!("Config: {}", config);
 }
