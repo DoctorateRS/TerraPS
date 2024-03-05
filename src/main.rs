0b8c006e2f88ed3cc790f156bc15532a81309fbe
Rh4096 <rhelvetican@gmail.com>
2024-03-05 18:56:46 +0700

    mangled

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ mod user;
 mod utils;
 
 use datapath::*;
+use init::dir_init;
 use utils::read_json;
 
 #[tokio::main]
 async fn main() {
+    dir_init();
     let config = read_json(CONFIG_JSON_PATH).unwrap();
 
     println!("{:?}", config)
