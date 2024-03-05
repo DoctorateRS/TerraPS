5127dafa11d49605fd993cd2a09a72eef803cc6e
Rh4096 <rhelvetican@gmail.com>
2024-03-05 13:10:39 +0700

    test

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,19 @@ mod config;
 mod routing;
 mod utils;
 
+use serde_json::{to_string_pretty, Value};
+
 use config::load_config;
+use utils::write_json;
 
 #[tokio::main]
 async fn main() {
-    let config = load_config();
+    let mut config = load_config();
 
-    println!("{}", config);
+    config["dummy_data"] = Value::String("new_dummy_data".to_string());
+
+    match write_json("config/config2.json", config) {
+        Ok(_) => println!("Config updated"),
+        Err(e) => println!("Cannot update config: {}", e),
+    };
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
-use serde_json::{from_reader, to_writer, Result, Value};
+use serde::Serialize;
+use serde_json::{from_reader, ser::PrettyFormatter, to_writer_pretty, Result, Serializer, Value};
 use std::{fs::File, io::BufReader};
 
 pub fn read_json(path: &str) -> Result<Value> {
@@ -6,7 +7,11 @@ pub fn read_json(path: &str) -> Result<Value> {
     from_reader(json_reader)
 }
 
-pub fn write_json(path: &str, value: &Value) -> Result<()> {
+pub fn write_json(path: &str, value: Value) -> Result<()> {
     let file = File::create(path).unwrap();
-    to_writer(file, value)
+    let fmt = PrettyFormatter::with_indent(b"   ");
+    let mut buf = Vec::new();
+    let mut ser = Serializer::with_formatter(&mut buf, fmt);
+    value.serialize(&mut ser).unwrap();
+    to_writer_pretty(file, &value)
 }
