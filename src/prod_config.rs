d381446607710f5eb76c0c2df41a2605eaa1c382
Rh4096 <rhelvetican@gmail.com>
2024-03-06 14:14:35 +0700

    decond

--- src/prod_config.rs
+++ src/prod_config.rs
@@ -2,9 +2,9 @@ use crate::{
     datapath::CONFIG_JSON_PATH,
     utils::{read_json, update_data},
 };
-
+use axum::response::Response;
 use rand::seq::SliceRandom;
-use serde_json::Value;
+use serde_json::{json, Value};
 
 fn random_hash() -> String {
     let mut pool = "abcdef".chars().collect::<Vec<char>>();
@@ -13,9 +13,8 @@ fn random_hash() -> String {
     pool.iter().collect::<String>()
 }
 
-pub async fn prod_refresh_config() {
-    // data = {"resVersion": None}
-    // return data, 200 <- todo: figure out what the fuck is this
+pub async fn prod_refresh_config() -> Response<Value> {
+    return Response::builder().status(200).body(json!({"resVersion": Value::Null})).unwrap();
 }
 
 pub async fn prod_network_config() {
@@ -23,14 +22,14 @@ pub async fn prod_network_config() {
     let _server = format!("http://{}:{}", server_config["server"]["host"], server_config["server"]["port"]).as_str();
 }
 
-pub async fn prod_remote_config() -> Value {
-    read_json(CONFIG_JSON_PATH).unwrap()["remote"].clone()
+pub async fn prod_remote_config() -> Response<Value> {
+    Response::builder().body(read_json(CONFIG_JSON_PATH).unwrap()["remote"].clone()).unwrap()
 }
 
-pub async fn prod_pre_announcement() -> Value {
-    update_data("https://ak-conf.hypergryph.com/config/prod/announce_meta/Android/preannouncement.meta.json").await
+pub async fn prod_pre_announcement() -> Response<Value> {
+    Response::builder().body(update_data("https://ak-conf.hypergryph.com/config/prod/announce_meta/Android/preannouncement.meta.json").await).unwrap()
 }
 
-pub async fn prod_announcement() -> Value {
-    update_data("https://ak-conf.hypergryph.com/config/prod/announce_meta/Android/announcement.meta.json").await
+pub async fn prod_announcement() -> Response<Value> {
+    Response::builder().body(update_data("https://ak-conf.hypergryph.com/config/prod/announce_meta/Android/announcement.meta.json").await).unwrap()
 }
