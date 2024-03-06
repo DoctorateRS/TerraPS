2825df40b2e5b02a5643f5858f63885300f4b050
Rh4096 <rhelvetican@gmail.com>
2024-03-06 13:52:26 +0700

    nodeterra

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,15 @@ mod datapath;
 mod init;
 mod prod_config;
 mod routing;
-mod update;
 mod user;
 mod utils;
 
 use datapath::*;
 use init::dir_init;
-use update::update_config;
 use utils::read_json;
 
 #[tokio::main]
 async fn main() {
     dir_init();
     let config = read_json(CONFIG_JSON_PATH).unwrap();
-    update_config().await;
 }
