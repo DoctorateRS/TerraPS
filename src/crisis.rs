b1f30303bccd106b0e798758840f9c954aab3919
Rh4096 <rhelvetican@gmail.com>
2024-03-07 19:28:45 +0700

    community

--- src/crisis.rs
+++ src/crisis.rs
@@ -30,4 +30,14 @@ pub fn crisis_v2_battle_start(Json(request_data): Json<Value>) -> Response<Value
         .unwrap()
 }
 
-pub fn crisis_v2_battle_finish() {}
+pub fn crisis_v2_battle_finish() {
+    let battle_data = read_json(RUNE_JSON_PATH).unwrap();
+    let mapId = battle_data["mapId"].to_string();
+    let runeSlots = battle_data["runeSlots"].to_string();
+    let mut score: [u32; 6] = [0, 0, 0, 0, 0, 0];
+    let selected_crisis = read_json(CONFIG_JSON_PATH).unwrap()["crisisV2Config"]["selectedCrisis"].to_string();
+    let rune = read_json(format!("{CRISIS_V2_JSON_BASE_PATH}{selected_crisis}.json").as_str()).unwrap();
+
+    let mut node = json!({});
+    for slot in rune["info"]["mapDetailDataMap"][mapId]["nodeDataMap"].as_object().unwrap() {}
+}
--- src/prod_config.rs
+++ src/prod_config.rs
@@ -6,12 +6,12 @@ use axum::response::Response;
 use rand::seq::SliceRandom;
 use serde_json::{json, Value};
 
-fn random_hash() -> String {
-    let mut pool = "abcdef".chars().collect::<Vec<char>>();
-    let mut rng = rand::thread_rng();
-    pool.shuffle(&mut rng);
-    pool.iter().collect::<String>()
-}
+// fn random_hash() -> String {
+//     let mut pool = "abcdef".chars().collect::<Vec<char>>();
+//     let mut rng = rand::thread_rng();
+//     pool.shuffle(&mut rng);
+//     pool.iter().collect::<String>()
+// }
 
 pub async fn prod_refresh_config() -> Response<Value> {
     Response::builder().status(200).body(json!({"resVersion": Value::Null})).unwrap()
@@ -27,8 +27,12 @@ pub async fn prod_network_config() -> Response<Value> {
     let func_ver = server_config["networkConfig"][&mode]["content"]["funcVer"].as_str().unwrap();
     let mut network_config = server_config["networkConfig"][&mode].clone();
     for (index, url) in server_config["networkConfig"][&mode]["content"]["configs"][&func_ver]["network"].as_object().unwrap() {
-        if url.is_string() && url.as_str().unwrap().contains("{server}") {
-            network_config["content"]["configs"][&func_ver]["network"][index] = url.as_str().unwrap().replace("{server}", &server).into();
+        if url.is_string() {
+            if url.as_str().unwrap().contains("{server}") {
+                network_config["content"]["configs"][&func_ver]["network"][index] = url.as_str().unwrap().replace("{server}", &server).into();
+            }
+        } else {
+            todo!("Handle non-string network config.")
         }
     }
     Response::builder().body(network_config).unwrap()
@@ -36,14 +40,9 @@ pub async fn prod_network_config() -> Response<Value> {
 
 async fn network_config_p1() {
     let mut server_config = read_json(CONFIG_JSON_PATH).unwrap();
-    let mode = server_config["server"]["mode"].to_string();
 
     let version = if server_config["assets"]["autoUpdate"].as_bool().unwrap_or(false) {
-        if mode == "cn" {
-            update_data("https://ak-conf.hypergryph.com/config/prod/official/Android/version").await
-        } else {
-            todo!()
-        }
+        update_data("https://ak-conf.hypergryph.com/config/prod/official/Android/version").await
     } else {
         server_config["version"]["android"].clone()
     };
