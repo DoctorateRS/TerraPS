6bc0d4c93b2170227496ff16b052fb107b44a0f8
Rh4096 <rhelvetican@gmail.com>
2024-03-15 23:23:10 +0700

    destroy the past

--- src/assets.rs
+++
@@ -1,3 +0,0 @@
-pub fn assets() {
-    println!("assets");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-mod assets;
-mod crisis;
-mod datapath;
-mod init;
-mod prod_config;
-mod routing;
-mod user;
-mod utils;
-
-#[tokio::main]
-async fn main() {}
+fn main() {
+    // Rewrite in progress
+}
