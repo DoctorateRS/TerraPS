3ae82429681e780fec1bf38ce5cfdcd136e8e85c
Rh4096 <rhelvetican@gmail.com>
2024-04-03 15:49:18 +0700

    account login

--- src/account.rs
+++ src/account.rs
@@ -1,1 +1,18 @@
 use crate::core::JSON;
+use axum::{http::HeaderMap, Json};
+use serde_json::json;
+use uuid::Uuid;
+
+pub async fn account_login(header: HeaderMap) -> JSON {
+    let fallback_uid = Uuid::new_v4().to_string();
+    let uid = match header.get("Uid") {
+        Some(uid) => uid.to_str().unwrap_or(fallback_uid.as_str()),
+        None => fallback_uid.as_str(),
+    };
+    Json(json!({
+        "result": 0,
+        "uid": uid,
+        "secret": "yostar",
+        "serviceLicenseVersion": 0
+    }))
+}
