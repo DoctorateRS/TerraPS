e26a44ac2e22a4bf5507e5cedf1be5d1af18f309
Rh4096 <rhelvetican@gmail.com>
2024-04-03 13:59:33 +0700

    user funcs

--- src/core/user.rs
+++ src/core/user.rs
@@ -1,6 +1,11 @@
 use axum::Json;
 use serde_json::{json, Value};
 
+use crate::{
+    constants,
+    utils::{read_json, write_json},
+};
+
 pub async fn app_v1_config() -> Json<Value> {
     Json(json!({
         "status": 0,
@@ -80,6 +85,63 @@ pub async fn info_v1_basic() -> Json<Value> {
             "identityName": "doctorate",
             "isMinor": false,
             "isLatestUserAgreement": true
+        }
+    }))
+}
+
+pub async fn user_check_in() -> Json<Value> {
+    Json(json!({
+        "result": 0,
+        "playerDataDelta": {
+            "modified": {},
+            "deleted": {}
+        }
+    }))
+}
+
+pub async fn user_agreement() -> Json<Value> {
+    Json(json!({
+        "data": [
+            "lol idk"
+        ],
+        "version": "4.0.0"
+    }))
+}
+
+pub async fn auth_v1_token_by_phone_password() -> Json<Value> {
+    Json(json!({
+        "status": 0,
+        "msg": "OK",
+        "data": {
+            "token": "doctorate"
+        }
+    }))
+}
+
+pub async fn user_change_secretary(Json(payload): Json<Value>) -> Json<Value> {
+    let mut config = read_json(constants::config::CONFIG_JSON_PATH).clone();
+    let mut user_data = read_json(constants::user::USER_JSON_PATH).clone();
+    let _ = payload["charInstId"].as_str().unwrap();
+    let skin_id = payload["skinId"].as_str().unwrap();
+    let secretary = match skin_id.find('@') {
+        Some(_) => skin_id.split('@').collect::<Vec<&str>>()[0],
+        None => skin_id.split('#').collect::<Vec<&str>>()[0],
+    };
+    config["userConfig"]["secretary"] = Value::String(secretary.to_string());
+    config["userConfig"]["secretarySkinId"] = Value::String(skin_id.to_string());
+    user_data["user"]["status"]["secretarySkinId"] = Value::String(skin_id.to_string());
+    user_data["user"]["status"]["secretary"] = Value::String(secretary.to_string());
+    write_json(constants::config::CONFIG_JSON_PATH, config);
+    write_json(constants::user::USER_JSON_PATH, user_data);
+    Json(json!({
+        "playerDataDelta": {
+            "modified": {
+                "status": {
+                    "secretary": secretary,
+                    "secretarySkinId": skin_id,
+                }
+            },
+            "deleted": {}
         }
     }))
 }
