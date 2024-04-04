d638792bc11145428204067f76affb0cf89fba30
Rh4096 <rhelvetican@gmail.com>
2024-04-04 10:00:44 +0700

    questing

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,8 +1,8 @@
 pub mod account;
 pub mod background;
 pub mod building;
-pub mod char_manage;
+pub mod char_manager;
 pub mod crisis_manager;
 pub mod online;
-pub mod quest;
+pub mod quest_manager;
 pub mod social;
---
+++ src/game/quest_manager.rs
@@ -0,0 +1,55 @@
+pub mod quest {
+    use axum::Json;
+    use serde_json::json;
+
+    use crate::core::JSON;
+
+    pub async fn set_trap_squad(Json(payload): JSON) -> JSON {
+        let trap_domain_id = payload["trapDomainId"].as_str().unwrap();
+        let trap_squad = payload["trapSquad"].clone();
+
+        Json(json!({
+            "playerDataDelta": {
+                "modified": {
+                    "templateTrap": {
+                        "domains": {
+                            trap_domain_id: {
+                                "squad": trap_squad
+                            }
+                        }
+                    }
+                },
+                "deleted": {}
+            }
+        }))
+    }
+}
+
+pub mod bossrush {
+    use axum::Json;
+    use serde_json::json;
+
+    use crate::core::JSON;
+
+    pub async fn relic_select(Json(payload): JSON) -> JSON {
+        let activity_id = payload["activityId"].as_str().unwrap();
+        let relic_id = payload["relicId"].clone();
+
+        Json(json!({
+            "playerDataDelta": {
+                "modified": {
+                    "activity": {
+                        "BOSS_RUSH": {
+                            activity_id: {
+                                "relic": {
+                                    "select": relic_id
+                                }
+                            }
+                        }
+                    }
+                },
+                "deleted": {}
+            }
+        }))
+    }
+}
