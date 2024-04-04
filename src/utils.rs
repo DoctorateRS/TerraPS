b12694fff327028ea9a3803c2a13c0549b3c9d03
Rh4096 <rhelvetican@gmail.com>
2024-04-05 04:12:30 +0700

    coil

--- src/utils.rs
+++ src/utils.rs
@@ -120,17 +120,4 @@ pub fn min<T: PartialOrd>(a: T, b: T) -> T {
     }
 }
 
-pub fn from_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
-    (0..s.len())
-        .step_by(2)
-        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
-        .collect()
-}
 
-pub fn to_hex(bytes: &[u8]) -> String {
-    let mut s = String::with_capacity(bytes.len() * 2);
-    for &b in bytes {
-        write!(&mut s, "{:02x}", b).unwrap();
-    }
-    s
-}
