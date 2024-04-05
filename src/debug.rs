20fc5312b8ff51392336d9a99ed7ef5c21573a0e
Rh4096 <rhelvetican@gmail.com>
2024-04-05 09:58:14 +0700

    case study

--- src/debug.rs
+++ src/debug.rs
@@ -1,5 +1,7 @@
 // DEBUGGING FUNCTIONS HERE
 
+use std::fs::{read, read_to_string};
+
 use axum::Json;
 use serde_json::json;
 
@@ -8,13 +10,12 @@ use crate::utils::{
     json::JSON,
 };
 
-pub async fn decrypt_battle_data(Json(payload): JSON) -> JSON {
-    let login_time = payload["login_time"].as_u64();
-    let data = payload["data"].as_str().unwrap();
+pub async fn decrypt_battle_data() -> JSON {
+    let data = read_to_string("./encrypted/DATA_1.txt").unwrap();
 
     const LOG_TOKEN_KEY: &str = "pM6Umv*^hVQuB6t&";
 
-    let login_time = login_time.unwrap_or(1672502400);
+    let login_time = 1672502400;
     let ptr = &data.len() - 32;
     let iv = &data[ptr..];
     let data = &data[..ptr];
@@ -23,10 +24,12 @@ pub async fn decrypt_battle_data(Json(payload): JSON) -> JSON {
     let key = md5_digest(src.as_bytes());
 
     let battle_data = match from_hex(data) {
-        Ok(data) => match String::from_utf8(data) {
-            Ok(data) => data,
-            Err(e) => panic!("Error parsing UTF-8: {e}"),
-        },
+        Ok(data) => data,
+        Err(e) => panic!("Error parsing Integer:{}", e),
+    };
+
+    let iv = match from_hex(iv) {
+        Ok(data) => data,
         Err(e) => panic!("Error parsing Integer:{}", e),
     };
 
