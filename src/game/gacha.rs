a4763a578bf83309e1675f4a14ca12377c76e812
Rh4096 <rhelvetican@gmail.com>
2024-04-06 12:26:53 +0700

    tired

--- src/game/gacha.rs
+++ src/game/gacha.rs
@@ -1,29 +1,3 @@
-pub mod normal {
-    use serde_json::{json, Value};
+pub mod normal {}
 
-    use crate::{
-        constants::url::GACHA_TABLE_URL,
-        utils::{
-            game::update_data,
-            json::{get_keys, JSON},
-            random::sample,
-        },
-    };
-
-    pub async fn get_tags() -> Value {
-        let gacha_table = update_data(GACHA_TABLE_URL).await;
-        let mut all_tags = Vec::new();
-        for data in gacha_table["gachaTags"].as_array().unwrap() {
-            let tag_id = data["tagId"].as_u64().unwrap();
-            if tag_id < 1000 {
-                all_tags.push(tag_id);
-            }
-        }
-        let tags = sample(all_tags, 5);
-        json!(tags)
-    }
-
-    pub async fn build_tag_char_set(mode: &str) {
-        let mut tag_char_set = json!({});
-    }
-}
+pub mod advanced {}
