52f713e1ea694e540e3211dc999b243f3254bb90
Rh4096 <rhelvetican@gmail.com>
2024-04-07 06:42:37 +0700

    upstream data

--- src/core/asset.rs
+++ src/core/asset.rs
@@ -4,14 +4,10 @@ use reqwest::RequestBuilder;
 use serde::Deserialize;
 use serde_json::json;
 
-use crate::utils::json::JSON;
-
 #[derive(Deserialize)]
 pub struct Asset {
     pub name: String,
     pub hash: String,
 }
 
-pub async fn get_file(Path(asset): Path<Asset>) -> JSON {
-    Json(json!({"name": asset.name, "hash": asset.hash}))
-}
+pub async fn get_file(Path(asset): Path<Asset>) {}
