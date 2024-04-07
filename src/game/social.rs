2da921c573954deb9ba74c4eff02e0b5456561a4
Rh4096 <rhelvetican@gmail.com>
2024-04-07 10:42:54 +0700

    basics

--- src/game/social.rs
+++ src/game/social.rs
@@ -1,6 +1,47 @@
 use crate::utils::json::JSON;
 use axum::Json;
-use serde_json::json;
+use serde_json::{json, Value};
+
+pub async fn social_search_player(Json(payload): JSON) -> JSON {
+    let username = payload["idList"].as_array().unwrap()[0].clone();
+    Json(json!({
+        "players": [
+            {
+                "nickName": username,
+                "nickNumber": "6666",
+                "uid": "66666666",
+                "friendNumLimit": 50,
+                "serverName": "泰拉",
+                "level": 120,
+                "avatarId": "0",
+                "avatar": {},
+                "assistCharList": [
+                    Value::Null
+                ],
+                "lastOnlineTime": 0,
+                "medalBoard": {
+                    "type": "EMPTY",
+                    "custom": Value::Null,
+                    "template": Value::Null
+                },
+                "skin": {
+                    "selected": "nc_rhodes_default",
+                    "state": {}
+                }
+            }
+        ],
+        "friendStatusList": [
+            0
+        ],
+        "resultIdList": [
+            "66666666"
+        ],
+        "playerDataDelta": {
+            "modified": {},
+            "deleted": {}
+        }
+    }))
+}
 
 pub async fn social_set_assist_char_list(Json(payload): JSON) -> JSON {
     Json(json!({
