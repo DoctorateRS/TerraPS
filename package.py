12eb16beedeb4b2e9e8c791d9fc2584bcecb1cca
Rh4096 <rhelvetican@gmail.com>
2024-04-13 00:35:10 +0700

    null

--- package.py
+++ package.py
@@ -4,10 +4,9 @@ import py7zr
 cargo = toml.load("Cargo.toml")
 version = cargo["package"]["version"]
 
-with py7zr.SevenZipFile(f"packaged/TerraPS_{version}.7z", "a") as archive:
-    archive.write("target/release/TerraPS.exe", "TerraPS.exe")
+with py7zr.SevenZipFile(f"packaged/TerraPS_{version}.7z", "w") as archive:
+    archive.write("target/release/terra-ps.exe", "TerraPS.exe")
     archive.writeall("data", "data")
     archive.writeall("config", "config")
     archive.write("README.md", "README.md")
     archive.write("LICENSE", "LICENSE")
-    archive.write("CHANGELOG.md", "CHANGELOG.md")
