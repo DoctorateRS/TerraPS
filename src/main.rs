d62a3717b4a42b587b7c57b05d143a1e8e10e5b6
Rh4096 <rhelvetican@gmail.com>
2024-05-08 00:32:24 +0700

    void

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,21 @@ mod utils;
 use anyhow::Result;
 use constants::ascii::TITLE;
 use routes::routes;
-use utils::server::Server;
+use utils::{fmt::excel_fmt, server::Server};
 
 use crate::utils::server::get_server_address;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    excel_fmt()?;
+
     // TITLE
     println!(r#"{}"#, TITLE);
     println!("IN CASE YOU PAID MONEY FOR THIS, YOU'VE BEEN SCAMMED.");
     println!("       THIS IS A FREE AND OPEN SOURCE PROJECT.       ");
 
     // SERVER
+
     let (server_address, server_port) = get_server_address();
     let server = Server::new(server_address, server_port);
     Ok(server.serve(routes()).await?)
---
+++ src/utils/fmt.rs
@@ -0,0 +1,20 @@
+use anyhow::{Ok, Result};
+use std::fs::read_dir;
+
+use super::json::{read_json, write_json};
+
+pub fn excel_fmt() -> Result<()> {
+    let dir = read_dir("./data/excel")?;
+
+    for entry in dir {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_file() {
+            let v = read_json(path.to_str().unwrap());
+            write_json(path.to_str().unwrap(), v);
+        }
+    }
+
+    Ok(())
+}
