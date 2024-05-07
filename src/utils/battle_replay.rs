7b9e78933c0649cf77be147ff881b35a0810e332
Rh4096 <rhelvetican@gmail.com>
2024-05-07 20:43:25 +0700

    resolve Update `zip` to version `1.1.x` #12

--- src/utils/battle_replay.rs
+++ src/utils/battle_replay.rs
@@ -1,7 +1,7 @@
 use crate::utils::crypto::base64::decode_bytes;
 use anyhow::{Ok, Result};
 use serde_json::{from_str, Value};
-use zip::{ZipArchive, ZipWriter};
+use zip::{write::SimpleFileOptions, CompressionMethod, ZipArchive, ZipWriter};
 
 use std::io::{read_to_string, BufReader, Cursor, Write};
 
@@ -23,7 +23,8 @@ pub fn encrypt_battle_replay(data: Value) -> Result<String> {
     let buffer = Vec::new();
     let str = data.to_string();
     let mut zip_writer = ZipWriter::new(Cursor::new(buffer));
-    zip_writer.start_file("default_entry", Default::default())?;
+    let options = SimpleFileOptions::default().compression_method(CompressionMethod::Stored);
+    zip_writer.start_file("default_entry", options)?;
     zip_writer.write_all(str.as_bytes())?;
     let bytes = zip_writer.finish()?.into_inner();
     Ok(encode_bytes(bytes))
