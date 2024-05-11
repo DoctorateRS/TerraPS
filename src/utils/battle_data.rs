14c07e90752ac2bb3b32d56c8b4f6b65dffb007c
Rh4096 <rhelvetican@gmail.com>
2024-05-11 12:15:17 +0700

    t1

--- src/utils/battle_data.rs
+++ src/utils/battle_data.rs
@@ -36,13 +36,13 @@ impl BattleDataDecoder {
         Self { login_time }
     }
 
-    pub fn decrypt_battle_data(&self, mut data: String) -> Result<Value> {
-        let data = data.drain(..data.len() - 32);
+    pub fn decrypt_battle_data(&self, data: String) -> Result<Value> {
+        let (data, iv) = data.split_at(data.len() - 32);
         let data = decode(data)?;
         let mut src = LOG_TOKEN_KEY.to_string();
         src.push_str(&self.login_time.to_string());
         let key = md5_digest(src.as_bytes()).to_vec();
-        let iv = decode(&data)?;
+        let iv = decode(iv)?;
         let aes = Aes128CbcDec::new(key.as_slice().into(), iv.as_slice().into());
         let res = aes.decrypt_padded_vec_mut::<NoPadding>(data.as_slice())?;
         let json_string = String::from_utf8(res)?;
