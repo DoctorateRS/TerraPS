b5dedf552555bf847f5f22b0394b575b70edeed9
Rh4096 <rhelvetican@gmail.com>
2024-05-13 18:10:17 +0700

    fix shit

--- src/utils/battle_data.rs
+++ src/utils/battle_data.rs
@@ -4,8 +4,8 @@ use aes::{
 };
 use anyhow::Result;
 use cbc::Decryptor;
-use hex::decode;
-use serde_json::{from_str, Value};
+use ring::test::from_hex;
+use serde_json::{from_str, json, Value};
 
 use super::crypto::md5::md5_digest;
 
@@ -38,14 +38,24 @@ impl BattleDataDecoder {
 
     pub fn decrypt_battle_data(&self, data: String) -> Result<Value> {
         let (data, iv) = data.split_at(data.len() - 32);
-        let data = decode(data)?;
-        let mut src = LOG_TOKEN_KEY.to_string();
-        src.push_str(&self.login_time.to_string());
+        let src = LOG_TOKEN_KEY.to_string() + &self.login_time.to_string();
+        let data = from_hex(data).unwrap();
+        let iv = from_hex(iv).unwrap();
         let key = md5_digest(src.as_bytes());
-        let iv = decode(iv)?;
         let aes = Aes128CbcDec::new(key.as_slice().into(), iv.as_slice().into());
-        let res = aes.decrypt_padded_vec_mut::<NoPadding>(data.as_slice())?;
-        let json_string = String::from_utf8(res)?;
-        Ok(from_str(json_string.trim())?)
+        let res = match aes.decrypt_padded_vec_mut::<NoPadding>(&data) {
+            Ok(res) => res,
+            Err(_) => {
+                return Ok(from_str("{}")?);
+            }
+        };
+        let res = match String::from_utf8(res) {
+            Ok(res) => res,
+            Err(_) => {
+                return Ok(from_str("{}")?);
+            }
+        };
+        let res = res.replace('\u{8}', "");
+        Ok(from_str(&res).unwrap_or(json!({})))
     }
 }
