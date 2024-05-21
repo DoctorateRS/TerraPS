80f08b5e184c057fd340ffd7f2cbe071e5a046d7
Rh4096 <rhelvetican@gmail.com>
2024-05-21 18:07:33 +0700

    fmt

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,27 +1,33 @@
-// Not fully functional yet, frida crate is also giving me errors out of nowhere, need to figure out what's wrong
+use frida::{DeviceManager, Frida, SpawnOptions};
+use std::process::Command;
 
 fn main() {
-    let mut cmd_terminal = std::process::Command::new("cmd");
-    let output = cmd_terminal.args(["/C adb root"]);
-    println!("Result: {:?}", output);
-    let output = cmd_terminal.args(["/C adb connect 127.0.0.1:7555"]);
-    println!("Result: {:?}", output);
-    let output = cmd_terminal.args(["/C adb reverse tcp:8443 tcp:8443"]);
-    println!("Result: {:?}", output);
-    let output = cmd_terminal.args(["/C adb shell /data/local/tmp/frida-server &"]);
-    println!("Result: {:?}", output);
+    let mut cmd_terminal = Command::new("cmd");
 
-    let frida = unsafe { frida::Frida::obtain() };
-    let device_manager = frida::DeviceManager::obtain(&frida);
-    let mut device = device_manager.enumerate_all_devices().into_iter().find(|device| device.get_id() == "127.0.0.1:7555").unwrap();
-    match device.spawn(
-        "com.hypergryph.arknights",
-        &frida::SpawnOptions::default(),
-    ) {
+    let cmds = vec![
+        "/C adb root",
+        "/C adb connect 127.0.0.1:7555",
+        "/C adb reverse tcp:8443 tcp:8443",
+        "/C adb shell /data/local/tmp/frida-server &",
+    ];
+
+    for cmd in cmds {
+        let output = cmd_terminal.args([cmd]);
+        println!("Result: {:?}", output);
+    }
+
+    let frida = unsafe { Frida::obtain() };
+    let device_manager = DeviceManager::obtain(&frida);
+    let mut device = device_manager
+        .enumerate_all_devices()
+        .into_iter()
+        .find(|device| device.get_id() == "127.0.0.1:7555")
+        .unwrap();
+    match device.spawn("com.hypergryph.arknights", &SpawnOptions::default()) {
         Ok(result) => {
             println!("PID: {}", result);
             device.resume(result).unwrap();
-    },
+        }
         Err(e) => println!("Error: {}", e),
     }
 }
