a6ac9227e97e95bd12ee0c380df3fd6c6ad69927
Rh4096 <rhelvetican@gmail.com>
2024-05-28 16:19:49 +0700

    feat: adding features

--- terraps/src/main.rs
+++ terraps/src/main.rs
@@ -6,22 +6,23 @@ mod utils;
 
 use anyhow::Result;
 use routes::routes;
-use utils::{
-    server::{get_server_address, Server},
-    upgrade,
-};
+
+#[cfg(feature = "server")]
+use utils::server::{get_server_address, Server};
+#[cfg(feature = "update")]
+use utils::upgrade;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // UPGRADE
+    #[cfg(feature = "update")]
     upgrade().await?;
 
-    // TITLE
-    println!("IN CASE YOU PAID MONEY FOR THIS, YOU'VE BEEN SCAMMED.");
-    println!("       THIS IS A FREE AND OPEN SOURCE PROJECT.       ");
-
-    // SERVER
-    let (server_address, server_port) = get_server_address();
-    let server = Server::new(server_address, server_port);
-    Ok(server.serve(routes()).await?)
+    #[cfg(feature = "server")]
+    {
+        println!("IN CASE YOU PAID MONEY FOR THIS, YOU'VE BEEN SCAMMED.");
+        println!("       THIS IS A FREE AND OPEN SOURCE PROJECT.       ");
+        let (server_address, server_port) = get_server_address();
+        let server = Server::new(server_address, server_port);
+        Ok(server.serve(routes()).await?)
+    }
 }
