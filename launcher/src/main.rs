fb6f628c2d561ca2cd9ff25dc95fedb1ea22e485
Rh4096 <rhelvetican@gmail.com>
2024-06-05 04:50:31 +0700

    fix

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -4,6 +4,7 @@ use frida::{DeviceManager, Frida, SpawnOptions};
 use std::process::Command;
 
 mod b64;
+mod init;
 use crate::b64::B64Decoder;
 
 const GLOBAL: &str = "Y29tLllvU3RhckVOLkFya25pZ2h0cw==";
@@ -15,10 +16,10 @@ fn main() -> Result<()> {
     let mut cmd_terminal = Command::new("cmd");
 
     let cmds = vec![
-        "/C adb root",
-        "/C adb connect 127.0.0.1:7555",
-        "/C adb reverse tcp:8443 tcp:8443",
-        "/C adb shell /data/local/tmp/frida-server &",
+        "/C adb.exe root",
+        "/C adb.exe connect 127.0.0.1:7555",
+        "/C adb.exe reverse tcp:8443 tcp:8443",
+        "/C adb.exe shell /data/local/tmp/frida-server &",
     ];
 
     for cmd in cmds {
