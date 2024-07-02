8efb0d74041c3d76bd7cbf3290c262563d8fe890
Rh4096 <rhelvetican@gmail.com>
2024-07-02 12:41:04 +0700

    poppler

--- terraps/src/main.rs
+++ terraps/src/main.rs
@@ -9,7 +9,6 @@ use std::env::args;
 use anyhow::Result;
 
 use routes::app;
-use tracing::info;
 use utils::{
     server::{get_server_address, Server},
     upgrade,
@@ -21,12 +20,12 @@ async fn main() -> Result<()> {
     let version = env!("CARGO_PKG_VERSION");
     upgrade().await?;
     if !arg_vec.contains(&"--upgrade".to_string()) {
-        info!("TerraPS {} is starting...", version);
+        println!("TerraPS {} is starting...", version);
         let (server_address, server_port) = get_server_address();
         let server = Server::new(server_address, server_port);
         server.serve(app()).await
     } else {
-        info!("TerraPS {} has been upgraded!", version);
+        println!("TerraPS {} has been upgraded!", version);
         Ok(())
     }
 }
