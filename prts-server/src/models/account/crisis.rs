68b66d02d1b9b8fdaebcbe2d427f57ea949114d8
Rh4096 <rhelvetican@gmail.com>
2024-08-05 15:50:43 +0700

    work

--- prts-server/src/models/account/crisis.rs
+++ prts-server/src/models/account/crisis.rs
@@ -20,22 +20,25 @@ impl Default for Crisis {
 #[derive(Deserialize, Serialize)]
 pub struct CrisisV2 {
     current: String,
-    seasons: HashMap<String, ()>,
+    seasons: HashMap<String, CrisisV2Season>,
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct CrisisV2Season {
-    permanent: String,
-    temporary: HashMap<String, ()>,
+    permanent: CrisisV2SeasonPermanent,
+    temporary: HashMap<String, CrisisV2SeasonTemporary>,
+    social: CrisisV2SeasonSocial,
 }
+
+type NullVec = [u8; 0];
 
 #[derive(Deserialize, Serialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct CrisisV2SeasonPermanent {
     state: u8,
-    score_total: Vec<u16>,
-    score_single: Vec<u16>,
-    comment: [u8; 0],
+    pub score_total: Vec<u16>,
+    pub score_single: Vec<u16>,
+    comment: NullVec,
     rune: EmptyMap,
     ex_runes: HashMap<String, u8>,
     rune_pack: EmptyMap,
@@ -47,7 +50,15 @@ pub struct CrisisV2SeasonPermanent {
 #[serde(rename_all = "camelCase")]
 pub struct CrisisV2SeasonTemporary {
     state: u8,
-    score_total: Vec<u16>,
+    pub score_total: Vec<u16>,
     rune: EmptyMap,
     challenge: EmptyMap,
+}
+
+#[derive(Deserialize, Serialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct CrisisV2SeasonSocial {
+    assist_cnt: u8,
+    max_pnt: i8,
+    chars: NullVec,
 }
