d25e1502bb4182882a464d3238edad50fae96377
Rh4096 <rhelvetican@gmail.com>
2024-08-30 22:34:19 +0700

    corelib2 -> transfer_protocol_2-013.1

--- crates/surtrps/src/models/prod/network.rs
+++ crates/surtrps/src/models/prod/network.rs
@@ -1,7 +1,108 @@
-use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, io::BufWriter};
 
-use crate::{cnst::config::CONFIG_PATH, SERVER_CONFIG};
+use serde::{Deserialize, Serialize};
+use serde_json::{from_value, Serializer as JsonSer};
+
+use crate::SERVER_CONFIG;
 use common_utils::read_json;
+
+#[derive(Serialize, Deserialize)]
+struct ProdAndroidNetworkConfig {
+    sign: String,
+    pub content: NetworkConfigContent,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+struct NetworkConfigContent {
+    config_ver: String,
+    pub configs: HashMap<String, NwCfg>,
+    func_ver: String,
+}
+
+impl NetworkConfigContent {
+    fn process(mut self, server: &str) -> Self {
+        let cfgv = self.config_ver;
+        let fv = self.func_ver;
+        let cfg = self.configs.remove(&fv);
+        if let Some(cfg) = cfg {
+            let ord = cfg.ord;
+            let secure = cfg.network.secure;
+            let sl = cfg.network.sl;
+            let of = cfg.network.of;
+            let gs = cfg.network.gs.replace("{server}", server);
+            let ak_server = cfg.network.ak_server.replace("{server}", server);
+            let ue = cfg.network.ue.replace("{server}", server);
+            let hu = cfg.network.hu.replace("{server}", server);
+            let hv = cfg.network.hv.replace("{server}", server);
+            let rc = cfg.network.rc.replace("{server}", server);
+            let an = cfg.network.an.replace("{server}", server);
+            let prean = cfg.network.prean.replace("{server}", server);
+            NetworkConfigContent {
+                config_ver: cfgv,
+                configs: {
+                    let mut cfgs = HashMap::new();
+                    cfgs.insert(
+                        fv.clone(),
+                        NwCfg {
+                            ord,
+                            network: NwCfgContent {
+                                gs,
+                                ak_server,
+                                ue,
+                                hu,
+                                hv,
+                                rc,
+                                an,
+                                prean,
+                                sl,
+                                of,
+                                pkg_ad: (),
+                                pkg_ios: (),
+                                secure,
+                            },
+                        },
+                    );
+                    cfgs
+                },
+                func_ver: fv,
+            }
+        } else {
+            NetworkConfigContent {
+                config_ver: String::from("5"),
+                configs: HashMap::new(),
+                func_ver: String::from("052"),
+            }
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct NwCfg {
+    pub network: NwCfgContent,
+    #[serde(rename = "override")]
+    ord: bool,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+struct NwCfgContent {
+    pub gs: String,
+    #[serde(rename = "as")]
+    pub ak_server: String,
+    #[serde(rename = "u8")]
+    pub ue: String,
+    pub hu: String,
+    pub hv: String,
+    pub rc: String,
+    pub an: String,
+    pub prean: String,
+    pub sl: String,
+    pub of: String,
+    pkg_ad: (),
+    pkg_ios: (),
+    secure: bool,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct ProdAndroidNetwork {
@@ -9,64 +110,26 @@ pub struct ProdAndroidNetwork {
     content: String,
 }
 
-#[derive(Serialize, Deserialize)]
-struct V0531 {
-    network: Network1,
-    r#override: bool,
-}
-#[derive(Serialize, Deserialize)]
-struct Configs1 {
-    V053: V0531,
-}
-#[derive(Serialize, Deserialize)]
-struct Content1 {
-    configVer: String,
-    configs: Configs1,
-    funcVer: String,
-}
-#[derive(Serialize, Deserialize)]
-struct Root1 {
-    content: Content1,
-    sign: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Network1 {
-    an: String,
-    #[serde(rename = "as")]
-    ax: String,
-    gs: String,
-    hu: String,
-    hv: String,
-    of: String,
-    pkgAd: (),
-    pkgIOS: (),
-    prean: String,
-    rc: String,
-    secure: bool,
-    sl: String,
-    u8: String,
-}
-
 impl ProdAndroidNetwork {
     pub fn load() -> Self {
-        let mode = SERVER_CONFIG.mode.as_str();
+        let mode = SERVER_CONFIG.mode.to_lowercase();
         let host = SERVER_CONFIG.host.as_str();
         let port = SERVER_CONFIG.port;
 
-        let cfg = read_json(CONFIG_PATH);
-
         let server = format!("http://{}:{}", host, port);
-        let func_ver = cfg["networkConfig"][&mode]["content"]["funcVer"].as_str().unwrap_or("");
 
-        let mut network_config = cfg["networkConfig"][&mode].clone();
-        for (index, url) in cfg["networkConfig"][&mode]["content"]["configs"][&func_ver]["network"].as_object().unwrap() {
-            if url.is_string() && url.as_str().unwrap().contains("{server}") {
-                network_config["content"]["configs"][&func_ver]["network"][index] = url.as_str().unwrap().replace("{server}", &server).into();
-            }
-        }
+        let cfg = from_value::<HashMap<String, ProdAndroidNetworkConfig>>(read_json("./config/network.json")).unwrap().remove(&mode).unwrap();
 
-        let content = network_config["content"].to_string();
+        let content = cfg.content.process(&server);
+
+        let content = {
+            let mut buf = Vec::<u8>::new();
+
+            let mut ser = JsonSer::new(&mut buf);
+            content.serialize(&mut ser).unwrap();
+
+            String::from_utf8(buf).unwrap()
+        };
 
         Self { sign: String::from("sign"), content }
     }
